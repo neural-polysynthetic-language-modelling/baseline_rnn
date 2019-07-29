ba088ca52d2345bb722be4719dd9e61a22be85d6
Hayley Park <hyunjipark3@gmail.com>
2019-07-29 21:57:29 +0000

    removing extra space character at the end

--- fst_data/ess_fst_char_preprocess.py
+++ fst_data/ess_fst_char_preprocess.py
@@ -8,17 +8,28 @@ def preprocess_fst(in_file, out_file):
 		with open(out_file, "w") as fst_preprocessed:
 			for line in fst_analysis:
 				tokens = line.split()
+				n_tokens = len(tokens)
+				token_idx = 0
 				for token in tokens:
 					if token.startswith("*"):
 						new_token = token[1:] # remove *
-						new_character_token = ' '.join(list(new_token)) # tokenize it by character 
-						new_character_token = new_character_token.replace("*", "") # remove extra asterisks
-						fst_preprocessed.write(new_character_token + " _ ")
+						new_token = ' '.join(list(new_token)) # tokenize it by character 
+						new_token = new_token.replace("*", "") # remove extra asterisks
+						if token_idx != n_tokens-1:
+							fst_preprocessed.write(new_token + " _ ")
+						else:
+							fst_preprocessed.write(new_token)
 					else:
 						new_token = token.split(":")[-2] # get the fst segmentation 
 						new_token = new_token.replace("^", " ")
 						new_token = new_token.replace("*", "") # remove extra asterisks
-						fst_preprocessed.write(new_token + " _ ")
+						if token_idx != n_tokens-1:
+							fst_preprocessed.write(new_token + " _ ")
+						else:
+							fst_preprocessed.write(new_token)
+
+
+					token_idx += 1
 
 				fst_preprocessed.write("\n")
 
